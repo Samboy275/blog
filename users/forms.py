740b337ebc9256e457f59758f04be0567c779708
samual moahmed <sam-wal@windowslive.com>
2022-03-09 16:43:10 +0200

    new changes

---
+++ users/forms.py
@@ -0,0 +1,16 @@
+import email
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class SignUpForm(UserCreationForm):
+    """User creation form"""
+
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
