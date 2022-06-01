301ffe5b91ddc1a915a91be1abefa3f1bace05e5
samual moahmed <sam-wal@windowslive.com>
2022-06-01 17:47:29 +0200

    Changed databse to postgres

--- blog/settings.py
+++ blog/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("DB_ENGINE"),
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST' : os.environ.get("DB_HOST"),
+        'PORT' : os.environ.get("DB_PORT")
     }
 }
 
