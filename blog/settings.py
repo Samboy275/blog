4b10b3ae735347cd59717b271177cea24eec3d1d
samual moahmed <sam-wal@windowslive.com>
2022-06-27 21:34:04 +0200

    heroku debug = true

--- blog/settings.py
+++ blog/settings.py
@@ -156,7 +156,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_FROM_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_PORT = 25
 
 
 # Static files (CSS, JavaScript, Images)
